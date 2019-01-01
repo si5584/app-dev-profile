e674f6e330a33075a4175a98d921a115dbcf0bf9
Simon Lovelock <simon@simons-mbp.home>
2019-01-01 03:27:04 +0000

    initial setup

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Simon Lovelock on 01/01/2019.
+//  Copyright © 2019 Simon Lovelock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
